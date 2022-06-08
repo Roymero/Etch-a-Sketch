00637607d6af50aff4be6cb44be46a8b0c0209c9
Roy Romero <Romerorr94@gmail.com>
2022-06-08 19:39:54 -0400

    initial functions working properly

--- script.js
+++ script.js
@@ -1,14 +1,61 @@
-let griddyDefault = document.querySelector(".griddyDefault");
-griddyDefault.style.gridTemplateColumns = "repeat(16,1fr)";
-griddyDefault.style.gridTemplateRows = "repeat(16,1fr)";
 
+function createBoard(rows, column) {
+    let griddyDefault = document.querySelector(".griddyDefault");
+    let squares = griddyDefault.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    griddyDefault.style.gridTemplateColumns = `repeat(${column},1fr)`;
+    griddyDefault.style.gridTemplateRows = `repeat(${rows},1fr)`;
 
+    let totalBlocks = rows * column;
 
+    for (let i = 0; i < totalBlocks; i++) {
+        let square = document.createElement("div");
+        square.addEventListener("mouseover", ()=> {
+            square.style.backgroundColor = "rgb(112,112,112)"; 
+            square.style.opacity = "1";
+        });
 
-for(let i = 0; i < 256; i++){
-    let square = document.createElement("div");
-    square.style.backgroundColor = "#D8D8D8";
-    square.style.opacity = '1';
-  
-    griddyDefault.insertAdjacentElement("beforeend", square);
+        square.style.backgroundColor = "#D8D8D8";
+        square.style.opacity = '1';
+
+        griddyDefault.insertAdjacentElement("beforeend", square);
+    }
+
 }
+
+const smol = document.getElementById('smol');
+const med = document.getElementById('med');
+const lorge = document.getElementById('lorge');
+
+
+createBoard(32,44);
+
+
+smol.addEventListener("click", ()=>{
+    createBoard(16,22);
+})
+
+
+med.addEventListener("click", ()=>{
+    createBoard(32,44);
+})
+
+lorge.addEventListener("click", ()=>{
+    createBoard(64,88);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
